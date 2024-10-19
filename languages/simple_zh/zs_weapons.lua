45b796d2c8c3f3a9a30cb847f003d5ff7fbe0c57
LVLVLVBAO <lvlvlvbao@gmail.com>
2024-10-19 13:12:28 +0100

    copy templates from English folder

---
+++ languages/simple_zh/zs_weapons.lua
@@ -0,0 +1,29 @@
+-- TIER, NAME, DESCRIPTION, PS, SANDBOX, ZE, ETC.
+
+-- Guns, Ammo, Tools, Melee, Deployables, Throwables/Others.
+
+translate.AddLanguage("en", "English")
+
+-- [[ ZS Main Weapons/Melee ]]
+-- Tier 1
+-- Tier 2
+-- Tier 3
+-- Tier 4
+-- Tier 5
+-- Tier 6
+
+-- [[ Banished Weapons ]]
+-- Tier 1
+-- Tier 2
+-- Tier 3
+-- Tier 4
+-- Tier 5
+-- Tier 6
+
+-- [[ ZS Pointsave Weapons/Melee ]]
+-- Tier 1
+-- Tier 2
+-- Tier 3
+-- Tier 4
+-- Tier 5
+-- Tier 6
