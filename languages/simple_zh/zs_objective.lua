02161a4e24e39b3f44be2dadf40ef632c7f546b5
Fallen <101518577+mctrr@users.noreply.github.com>
2024-10-20 20:08:27 +0800

    same

--- languages/simple_zh/zs_objective.lua
+++ languages/simple_zh/zs_objective.lua
@@ -3,8 +3,8 @@
 translate.AddLanguage("en", "English")
 
 -- Objective Mode unlocks
-LANGUAGE.class_not_unlocked_yet_unlocked_in_x               = "That class is not unlocked yet. It will be unlocked in %s."
-LANGUAGE.class_is_locked_obj                                = "This class is locked in objective maps!"
-LANGUAGE.zombie_in_x                                        = "Wave %s zombies will be unlocked in %s"
-LANGUAGE.you_will_be_x_in_x                                 = "You will become %s in %s!"
-LANGUAGE.x_will_be_y_in_z                                   = "%s will become %s in %s!"
+LANGUAGE.class_not_unlocked_yet_unlocked_in_x               = "这个丧失种类还未解锁. 它将会在 %s 时解锁"
+LANGUAGE.class_is_locked_obj                                = "这个丧尸种类在Obj地图禁用"
+LANGUAGE.zombie_in_x                                        = "波数 %s 的丧尸会在 %s 内解锁"
+LANGUAGE.you_will_be_x_in_x                                 = "你将变成 %s 在 %s 的时候!"
+LANGUAGE.x_will_be_y_in_z                                   = " %s 将会变成 %s 在 %s 的时候!"
--- languages/simple_zh/zs_skills.lua
+++ languages/simple_zh/zs_skills.lua
@@ -2,12 +2,12 @@
 -- GAMEMODES: PS, SANDBOX, ETC.
 
 -- Skill system messages
-LANGUAGE.unspent_skill_points_press_x                       = "You have unspent skill points. Press %q to unlock new skills."
-LANGUAGE.x_requires_a_skill_you_dont_have                   = "%s requires a skill you don't have!"
-LANGUAGE.you_ascended_to_level_x                            = "You have ascended to level %d!"
-LANGUAGE.you_have_remorted_now_rl_x                         = "You have remorted and ascended to remort level %d!"
-LANGUAGE.you_now_have_x_extra_sp                            = "You now have %d extra skill points!"
-LANGUAGE.x_has_remorted_to_rl_y                             = "%s has remorted to remort level %d!!"
-LANGUAGE.you_have_reset_all                                 = "All of your skills, XP, and skill points have been reset."
+LANGUAGE.unspent_skill_points_press_x                       = "你有未使用的技能点数. 按 %q 来解锁新技能."
+LANGUAGE.x_requires_a_skill_you_dont_have                   = " %s 需要你先解锁上个技能! "
+LANGUAGE.you_ascended_to_level_x                            = "你升到了 %d 级!"
+LANGUAGE.you_have_remorted_now_rl_x                         = "你已经转生,你的转生等级为 %d !"
+LANGUAGE.you_now_have_x_extra_sp                            = "你现在有 %d 额外点数!"
+LANGUAGE.x_has_remorted_to_rl_y                             = " %s 已转生至 %d 级"
+LANGUAGE.you_have_reset_all                                 = "所有的技能和技能点数已被重置"
 
-LANGUAGE.unspent_dark_essence_press_x                       = "You have unspent Dark Essence. Press %q to open the Dark Market."
+LANGUAGE.unspent_dark_essence_press_x                       = "你有未使用的灵魂精华. 按 %q 打开灵魂市场使用"
